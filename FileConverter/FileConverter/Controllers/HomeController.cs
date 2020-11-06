22f1fab0104fe380d4a735ac624c3a10625b8c79
Kamran <kamranqdn@gmail.com>
2020-11-07 01:30:19 +0530

    Added files and CSV changes

--- FileConverter/FileConverter/Controllers/HomeController.cs
+++ FileConverter/FileConverter/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Aspose.Words;
 using FileConverter.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace FileConverter.Controllers
 {
@@ -16,6 +20,43 @@ namespace FileConverter.Controllers
         {
          
             return View();
+        }
+        public ActionResult  FirstCall(string[] args)
+        {
+            var json = @"{
+                       ""employees"": [
+                       { ""firstName"":""John"" , ""lastName"":""Doe"" },
+                       { ""firstName"":""Anna"" , ""lastName"":""Smith"" },
+                       { ""firstName"":""Peter"" , ""lastName"":""Jones"" }
+                       ]
+                       }";
+            GetDataTableFromJsonString(json);
+
+            return View();
+        }
+
+        public DataTable GetDataTableFromJsonString(string json)
+        {
+            var jsonLinq = JObject.Parse(json);
+
+            // Find the first array using Linq  
+            var srcArray = jsonLinq.Descendants().Where(d => d is JArray).First();
+            var trgArray = new JArray();
+            foreach (JObject row in srcArray.Children<JObject>())
+            {
+                var cleanRow = new JObject();
+                foreach (JProperty column in row.Properties())
+                {
+                    // Only include JValue types  
+                    if (column.Value is JValue)
+                    {
+                        cleanRow.Add(column.Name, column.Value);
+                    }
+                }
+                trgArray.Add(cleanRow);
+            }
+            var a = JsonConvert.DeserializeObject<DataTable>(trgArray.ToString());
+            return a;
         }
         public ActionResult FileUploadWord(HttpPostedFileBase postedFile)
         {
@@ -36,6 +77,28 @@ namespace FileConverter.Controllers
                 postedFile.SaveAs(fileSavePath.ToString());
                 Document doc = new Document(fileSavePath.ToString());
                 doc.Save(PDFfileSavePath.ToString() + ".pdf");
+            }
+            return View();
+        }
+        public ActionResult ConvertPDFintoDOCX(HttpPostedFileBase postedFile)
+        {
+            if (postedFile != null)
+            {
+                object documentFormat = 8;
+                string randomName = DateTime.Now.Ticks.ToString();
+                object htmlFilePath = Server.MapPath("~/HTMLWordFiles/") + randomName + ".htm";
+                string directoryPath = Server.MapPath("~/WordFiles/") + randomName + "_files";
+                object fileSavePath = Server.MapPath("~/WordFiles/") + Path.GetFileName(postedFile.FileName);
+                object PDFfileSavePath = Server.MapPath("~/PDFFiles/") + Path.GetFileName(postedFile.FileName);
+
+                //If Directory not present, create it.
+                if (!Directory.Exists(Server.MapPath("~/HTMLWordFiles/")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/HTMLWordFiles/"));
+                }
+                postedFile.SaveAs(PDFfileSavePath.ToString());
+                Document doc = new Document(fileSavePath.ToString());
+                doc.Save(fileSavePath.ToString() + ".doc");
             }
             return View();
         }
